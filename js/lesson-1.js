4278fe7ced7aa6a098e9ccb0a768e6ec14ee2c3d
Yurii <yerikstar@gmail.com>
2025-06-07 09:14:26 +0300

    add less-1 task-1

---
+++ js/lesson-1.js
@@ -0,0 +1,25 @@
+// Task-1
+// Запропонуйте користувачу ввести число за допомогою prompt().
+// Збережіть введене значення у змінну number.
+// Перевірте, чи дорівнює змінна number числу 10:
+//    Якщо так – виведіть повідомлення 'Вірно' у alert().
+//    Якщо ні – виведіть 'Невірно'. 
+
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою  
+// перетворіть значення на число за допомогою Number().
+
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return 'Insufficient funds!';
+    }
+    else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+}
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
