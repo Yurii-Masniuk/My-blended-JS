351e3342055988714d33365bcb406be527499f1f
Yurii <yerikstar@gmail.com>
2025-06-07 09:33:00 +0300

    add less-1 task-1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,25 +1,23 @@
+// !===================
+
 // Task-1
 // Запропонуйте користувачу ввести число за допомогою prompt().
 // Збережіть введене значення у змінну number.
 // Перевірте, чи дорівнює змінна number числу 10:
 //    Якщо так – виведіть повідомлення 'Вірно' у alert().
-//    Якщо ні – виведіть 'Невірно'. 
+//    Якщо ні – виведіть 'Невірно'.
 
-// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою  
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
 // перетворіть значення на число за допомогою Number().
 
+// !===================
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits) {
-        return 'Insufficient funds!';
-    }
-    else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+let number = prompt('Введіть число');
+number = Number(number);
+if (number === 10) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+
+// !===================
