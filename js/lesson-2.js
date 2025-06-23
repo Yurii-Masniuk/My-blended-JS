357fbed9bf97bf6916e41d7f260a067be41db952
Yurii <yerikstar@gmail.com>
2025-06-23 23:06:09 +0300

    add task-8

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -156,6 +156,13 @@
 //     Poly: 160,
 //     Ajax: 1470,
 //   };
+// let totalSalary = 0;
+// const values = Object.values(salaries);
+// for (const value of values) {
+//     totalSalary += value;
+// }
+// console.log(totalSalary);
+
 
 // !===================
 // Task-9
