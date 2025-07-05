418d7abbdef9f206ccb372281f25c99a38cd6adf
Yurii <yerikstar@gmail.com>
2025-07-05 19:51:06 +0300

    add less-3 task-6

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -61,6 +61,12 @@
 // !===================
 
 // Task-6
+// Відсортуйте масив чисел [4, 2, 5, 1, 3] 
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+// numbersArray.sort((a, b) => a - b);
+// console.log(numbersArray);
 
 // !===================
 
@@ -95,82 +101,79 @@
 // !===================
 
 // Task-11
- // Розроби клас Calculator, який дозволяє виконувати арифметичні 
- //  операції над числом за допомогою методів класу, підтримуючи  
- // ланцюжковий виклик (method chaining).
- //
- // Вимоги до класу Calculator
- // - Метод number(value)
- // Встановлює початкове значення для наступних обчислень.
- // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
- 
- // - метод getResult, Повертає поточний результат усіх операцій.
- // Не змінює значення, просто повертає його.
- 
- // - метод add - Додає value до поточного значення.
- // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
- 
- // - метод substruct - Віднімає value від поточного значення. Повертає this.
- 
+// Розроби клас Calculator, який дозволяє виконувати арифметичні
+// операції над числом за допомогою методів класу, підтримуючи
+// ланцюжковий виклик (method chaining).
+//
+// Вимоги до класу Calculator
+// - Метод number(value)
+// Встановлює початкове значення для наступних обчислень.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод getResult, Повертає поточний результат усіх операцій.
+// Не змінює значення, просто повертає його.
+
+// - метод add - Додає value до поточного значення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод substruct - Віднімає value від поточного значення. Повертає this.
+
 // - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
 // Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
 // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
- 
- // - метод multiply -Множить поточне значення на value. Повертає this.
- 
- // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
- 
- // Приклад використання:
- // const calc = new Calculator();
 
+// - метод multiply -Множить поточне значення на value. Повертає this.
+
+// Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+
+// Приклад використання:
+// const calc = new Calculator();
 
 // const result = calc
-//   .number(10)   // Встановлюємо початкове значення 10
-//   .add(5)       // Додаємо 5 (10 + 5 = 15)
-//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
-//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
-//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
-//   .getResult(); // Отримуємо результат: 24
+// .number(10) // Встановлюємо початкове значення 10
+// .add(5) // Додаємо 5 (10 + 5 = 15)
+// .subtract(3) // Віднімаємо 3 (15 - 3 = 12)
+// .multiply(4) // Множимо на 4 (12 * 4 = 48)
+// .divide(2)  // Ділимо на 2 (48 / 2 = 24)
+// .getResult(); // Отримуємо результат: 24
+// console.log(result); // 24
 
-
-//   console.log(result); // 24
-
-class Calculator {
-    number(value) {
-        this.value = value;
-        return this;
-    }
-    getResult() {
-        return this.value;
-    }
-    add(value) {
-        this.value += value;
-        return this;
-    }
-    subtract(value) {
-        this.value -= value;
-        return this;
-      }
+// class Calculator {
+//     number(value) {
+//         this.value = value;
+//         return this;
+//     }
+//     getResult() {
+//         return this.value;
+//     }
+//     add(value) {
+//         this.value += value;
+//         return this;
+//     }
+//     subtract(value) {
+//         this.value -= value;
+//         return this;
+//       }
     
-      multiply(value) {
-        this.value *= value;
-        return this;
-    }
-    divide(value) {
-        this.value /= 2;
-        return this;
-      }
-}
-const calc = new Calculator();
-const result = calc
-  .number(10)
-  .add(5)
-  .subtract(3)
-  .multiply(4)
-  .divide(2)
-  .getResult();
+//       multiply(value) {
+//         this.value *= value;
+//         return this;
+//     }
+//     divide(value) {
+//         this.value /= 2;
+//         return this;
+//       }
+// }
+// const calc = new Calculator();
+// const result = calc
+//   .number(10)
+//   .add(5)
+//   .subtract(3)
+//   .multiply(4)
+//   .divide(2)
+//   .getResult();
 
-console.log(result);
+// console.log(result);
 
   
     
