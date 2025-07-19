6cec113746bc3cf6a0b9b33e34e55b4764d1cb84
Yurii <yerikstar@gmail.com>
2025-07-19 16:07:26 +0300

    add less-4

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,15 +1,44 @@
 // !===================
 // Task-1
 // 1 - отримай body елемент і виведи його в консоль;
+const body = document.querySelector('body');
+console.log(body);
 // 2 - отримай елемент id="title" і виведи його в консоль;
+const title = document.querySelector('#title');
+console.log(title);
+
 // 3 - отримай елемент class="list" і виведи його в консоль;
+const list = document.querySelector('.list');
+console.log(list);
 // 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
+const dataTopics = document.querySelectorAll('[data-topic]');
+console.log(dataTopics);
+
 // 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const firstDataTopic = dataTopics[0];
+console.log(firstDataTopic);
 // 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic і виведи його в консоль;
+const lastDataTopic = dataTopics[dataTopics.length - 1];
+console.log(lastDataTopic);
+
 // 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
+
+const titleNextSibling = title.nextElementSibling;
+console.log(titleNextSibling);
+
 // 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
+const allTitles = document.querySelectorAll('h3');
+console.log(allTitles);
+
 // 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка на червоний колір
+allTitles.forEach(item => {
+    item.classList.add('active');
+});
+
 // 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+const navItemEl = document.querySelector('li[data-topic="navigation"]');
+console.log(navItemEl);
+
 // 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
 // 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
 // 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
@@ -20,6 +49,9 @@
 // 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
+
+
+
 
 // !===================
 // Task-2
